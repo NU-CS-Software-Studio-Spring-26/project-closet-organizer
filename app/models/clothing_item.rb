06194ec6a7446e4403dd6d76e714b2db7edda5f9
Adedamola Adejumobi <a.adejumobi67@gmail.com>
2026-04-19 17:57:37 -0500

    Add User and ClothingItem models with seed data

---
+++ app/models/clothing_item.rb
@@ -0,0 +1,11 @@
+class ClothingItem < ApplicationRecord
+  belongs_to :user
+
+  enum :size, {
+    xs: 0,
+    small: 1,
+    medium: 2,
+    large: 3,
+    xl: 4
+  }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_secure_password 
+
+    has_many :clothing_items, dependent: :destroy 
+end
