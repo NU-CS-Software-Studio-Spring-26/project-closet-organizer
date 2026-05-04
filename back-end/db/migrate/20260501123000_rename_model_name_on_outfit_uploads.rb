fb8df373e97ddc48ea46450efc222ffe5abc9e74
AnnabelGoldman <annabel@crossplaydigital.com>
2026-05-03 19:26:47 -0500

    Hotfixing migration patterns for prod deployment

--- back-end/db/migrate/20260501123000_rename_model_name_on_outfit_uploads.rb
+++ back-end/db/migrate/20260501123000_rename_model_name_on_outfit_uploads.rb
@@ -1,5 +1,8 @@
 class RenameModelNameOnOutfitUploads < ActiveRecord::Migration[8.1]
   def change
+    return unless column_exists?(:outfit_uploads, :model_name)
+    return if column_exists?(:outfit_uploads, :vision_model)
+
     rename_column :outfit_uploads, :model_name, :vision_model
   end
 end
