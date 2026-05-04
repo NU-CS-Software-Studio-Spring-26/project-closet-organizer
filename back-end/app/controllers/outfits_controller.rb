1888491f767c801d9bc1bbb41269378baaa2a62b
rsmkhal <133159514+rsmkhal@users.noreply.github.com>
2026-05-03 23:58:50 -0500

    feat: add outfits CRUD and draft-based outfit builder

---
+++ back-end/app/controllers/outfits_controller.rb
@@ -0,0 +1,68 @@
+class OutfitsController < ApplicationController
+  before_action :require_login
+  before_action :set_outfit, only: %i[ show update destroy ]
+
+  def index
+    outfits = current_user.outfits.includes(:clothing_items).order(created_at: :desc)
+    render json: outfits.map { |outfit| outfit_payload(outfit) }
+  end
+
+  def show
+    render json: outfit_payload(@outfit)
+  end
+
+  def create
+    outfit = current_user.outfits.new(outfit_attributes)
+    assign_items(outfit)
+
+    if outfit.errors.empty? && outfit.save
+      render json: outfit_payload(outfit), status: :created
+    else
+      render_validation_errors(outfit)
+    end
+  end
+
+  def update
+    @outfit.assign_attributes(outfit_attributes)
+    assign_items(@outfit)
+
+    if @outfit.errors.empty? && @outfit.save
+      render json: outfit_payload(@outfit)
+    else
+      render_validation_errors(@outfit)
+    end
+  end
+
+  def destroy
+    @outfit.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_outfit
+    @outfit = current_user.outfits.includes(:clothing_items).find(params[:id])
+  end
+
+  def outfit_params
+    params.require(:outfit).permit(:name, :notes, tags: [], item_ids: [])
+  end
+
+  def outfit_attributes
+    outfit_params.slice(:name, :notes, :tags)
+  end
+
+  def assign_items(outfit)
+    return unless outfit_params.key?(:item_ids)
+
+    item_ids = Array(outfit_params[:item_ids]).reject(&:blank?).map(&:to_i).uniq
+    items = current_user.clothing_items.where(id: item_ids)
+
+    if items.size != item_ids.size
+      outfit.errors.add(:item_ids, "contain items you do not own")
+      return
+    end
+
+    outfit.clothing_items = items
+  end
+end
